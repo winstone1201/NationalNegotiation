fe73c45a0fb8cc1fb267e50dfdc8033de337fca4
winstone1201 <winstone1201@gmail.com>
2026-07-02 18:19:38 +0800

    feat: 视频海报封面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@
           controls
           preload="metadata"
           playsinline
-          poster="/视频封面.jpg"
+          poster="/video-cover.jpg"
         >
           <source src="/videos/negotiation-highlights.mp4" type="video/mp4" />
           您的浏览器不支持视频播放，请升级浏览器。
